4e2129f8e510285b4ff9a5e9dc0ce5e6c181f85d
betojs <albertoj2504@gmail.com>
2019-08-21 14:07:35 -0400

    Funcionando

--- controladores/calculos/kalmanfilter.js
+++ controladores/calculos/kalmanfilter.js
@@ -1,10 +1,8 @@
 const RawData = require('../../models/rawdata');
+const Distancia = require('../calculos/getdistance')
 
 
 
-// var vector = new Array();
-// let kalmanVariance = 8.184352498;       //var REAL
-// let kalmanCovariance = 0.05;
 
 // vector[i].Q = kalmanCovariance;
 // vector[i].R = kalmanVariance;
@@ -17,47 +15,53 @@ const RawData = require('../../models/rawdata');
 // vector[i].kalmanT= 0.0;        //kt^
 
 var prueba=0;
-let filtrado = (cont) =>{
+let filtrado = async ( macrpi, mactag) =>{
+
 
     console.log(`estoy filtrando`);
-    vector[i].Q = kalmanCovariance;
-    vector[i].R = kalmanVariance;
-    vector[i].Xt_predictiva = 0.0; //xt~
-    vector[i].Xt_1 = -65.0;          // x~1           
-    vector[i].Xt_s= -65.0;           //xt^
-    vector[i].P_Covar_pre= 0.0;    //pt~
-    vector[i].P_Covar_1= 0.0;      //pt~1   
-    vector[i].P_Covar_s= 0.0;      //pt^
-    vector[i].kalmanT= 0.0;        //kt^
-    RawData.find().limit(5).sort({_id:-1})
-        .exec(function (err, device) {
-            if (err) {
-                return res.status(400).json({
-                    ok: false,
-                    err
-                });
-            }
-        
-        prueba= device[cont].rssi + 10;
-        for(let i = (device.length)-1; i >-1; i--){
-             
-
-
-            meshEvaluate[i].Xt_predictiva = meshEvaluate[i].Xt_1;
-            meshEvaluate[i].P_Covar_pre = meshEvaluate[i].P_Covar_1 + meshEvaluate[i].Q;
-            meshEvaluate[i].kalmanT = (meshEvaluate[i].P_Covar_pre)/(meshEvaluate[i].P_Covar_pre + meshEvaluate[i].R);
-            meshEvaluate[i].Xt_s = meshEvaluate[i].Xt_predictiva + meshEvaluate[i].kalmanT *(meshEvaluate[i].rssi- meshEvaluate[i].Xt_predictiva);
-            meshEvaluate[i].P_Covar_s = (1-meshEvaluate[i].kalmanT)*meshEvaluate[i].P_Covar_pre; 
-            meshEvaluate[i].Xt_1 = meshEvaluate[i].Xt_s; 
-            meshEvaluate[i].P_Covar_1 = meshEvaluate[i].P_Covar_s;                             
-
-            console.log(`${device[i].rpidate}`);
+    
+    await RawData.find({macRpi:macrpi, macTag:mactag}).limit(5).sort({_id:-1})
+    .exec( (err, device) =>{
+        if (err) {
+            console.log(`Error`.red + err);
+            return 0;
         }
-        
+        if(device[device.length-1] !== undefined){
+            // console.log(`${}`);
+            let kalmanVariance = 8.184352498;       //var REAL
+            let kalmanCovariance = 0.05;
+            let Q = kalmanCovariance;
+            let R = kalmanVariance;
+            let Xt_predictiva = 0.0; //xt~
+            let Xt_1 = device[device.length-1].rssi;          // x~1           
+            let Xt_s= device[device.length-1].rssi;           //xt^
+            let P_Covar_pre= 0.0;    //pt~
+            let P_Covar_1= 0.0;      //pt~1   
+            let P_Covar_s= 0.0;      //pt^
+            let kalmanT= 0.0;        //kt^
+            console.log(`el primer dato es: ${Xt_1}`);
+            for(let i = (device.length)-1; i >-1; i--){
+                
+    
+    
+                Xt_predictiva = Xt_1;
+                P_Covar_pre = P_Covar_1 + Q;
+                kalmanT = (P_Covar_pre)/(P_Covar_pre + R);
+                Xt_s = Xt_predictiva + kalmanT *(device[i].rssi- Xt_predictiva);
+                P_Covar_s = (1-kalmanT)*P_Covar_pre; 
+                Xt_1 = Xt_s; 
+                P_Covar_1 = P_Covar_s;                             
+                    
+                // console.log(`${device[i].rpidate}`);
+            }
+            console.log(`rssi filtrado para macRpi:${macrpi} && macTag:${mactag}, es de rssi:${Xt_s}`);
+            // Distancia.distancia(mac, Xt_s)
+        }else{
+            console.log(`No se encontro Registros para macRpi:${macrpi} && macTag:${mactag}`);
+        }
 
     });
     
-
 
 }
 module.exports = {
--- controladores/calculos/timer.js
+++ controladores/calculos/timer.js
@@ -1,5 +1,8 @@
 const filtrar = require('./kalmanfilter')
 const colors = require ('colors')
+const RawData = require('../../models/rawdata');
+
+
 
 let tiempoEspera = () =>{
 
@@ -23,7 +26,39 @@ let tiempoScanner =async () =>{
         /* ****************************************/
         
         cont--;
-        await filtrar.filtrado(cont);
+        try{
+        RawData.aggregate([{"$group":{_id:"$macRpi", count:{$sum:1}}}])
+        .exec(function (err, macrpi){
+            if (err) {
+                console.log(`err`.blue + err);
+            }
+            RawData.aggregate([{"$group":{_id:"$macTag", count:{$sum:1}}}])
+            .exec(function (er, mactag){
+                if (er) {
+                    console.log(`err`.yellow + er);
+                }
+
+                console.log(macrpi.length);
+                console.log(macrpi.length);
+                for (let i= 0;i<macrpi.length;i++){
+                    for(let j=0; j<mactag.length; j++){
+                        try{
+                            filtrar.filtrado( macrpi[i]._id , mactag[j]._id );
+
+                        }catch(err){
+
+                            console.log(`ERROR:`.red + `${err}`);
+                        }
+
+                    }
+                }
+            });    
+
+
+        });
+    }catch(err){}
+
+        
     },5000);
     setTimeout(() => {
         console.log(`STOP`.red);
