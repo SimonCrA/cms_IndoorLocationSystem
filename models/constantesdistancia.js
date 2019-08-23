66de928e6920b7289cd95b6e5eac2122e5f4572a
betojs <albertoj2504@gmail.com>
2019-08-23 11:00:59 -0400

    todo bad

--- models/constantesdistancia.js
+++ models/constantesdistancia.js
@@ -4,13 +4,8 @@ var Schema = mongoose.Schema;
 
 var constantesDistanciaSchema = new Schema({
 
-<<<<<<< HEAD
 		macRpi: {type: String,  max: 100},
 		macTag: {type: String,  max: 100},
-=======
-		macrpi: {type: String, max: 50},
-		mactag: {type: String,  max: 50},
->>>>>>> 2539727928105f483957d9ca1a04ce90fe60f9a3
 		nPropagacion: {type: Number },        
         rssiProm: {type: Number},
 		desviacionEstandar: {type: Number}
