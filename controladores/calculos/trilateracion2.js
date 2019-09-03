ae2ec08c9cd7832edabf56b9bb77cf76453af75b
SimonCrA <csimonr28@gmail.com>
2019-09-03 17:21:45 -0400

    Modulo nuevo de trilateracion agregado.

---
+++ controladores/calculos/trilateracion2.js
@@ -0,0 +1,58 @@
+const matrix = require('matrix-js');
+
+
+let trilateracionMatriz = (data) =>{
+
+
+    var A = (-2 * data.x1 + 2 * data.x2);
+    var B = (-2 * data.y1 + 2 * data.y2);
+    var C = (Math.pow(data.r1, 2) - Math.pow(data.r2, 2) - Math.pow(data.x1, 2) + Math.pow(data.x2, 2) - Math.pow(data.y1, 2) + Math.pow(data.y2, 2));
+    
+    var D = (-2 * data.x2 + 2 * data.x3);
+    var E = (-2 * data.y2 + 2 * data.y3);
+    
+    var F = (Math.pow(data.r2, 2) - Math.pow(data.r3, 2) - Math.pow(data.x2, 2) + Math.pow(data.x3, 2) - Math.pow(data.y2, 2) + Math.pow(data.y3, 2));
+    
+    
+    var a = [
+        [A, B],
+        [D, E]
+    ];
+    var b = [
+        [C], 
+        [F]
+    ]
+    // console.log(a);
+    // console.log(b);
+    
+    var G = matrix(a);
+    var H = matrix(b);
+    
+    
+    var I = G.inv();
+    var J = matrix(I);
+    // console.log(J());
+    // console.log(H());
+    
+    var z = J.prod(H);
+    
+    
+    // console.log(z);
+
+    return {
+        x:z[0][0],
+        y:z[1][0]
+    };
+
+
+}    
+
+
+
+// console.log(trilateracion(data));
+
+module.exports = {
+    trilateracionMatriz
+}
+
+
--- controladores/calculos/validacion.js
+++ controladores/calculos/validacion.js
@@ -3,14 +3,19 @@ const Distancia = require('../../models/distancias');
 const colors = require('colors');
 const Ubicacion = require ('../../models/ubicacion')
 const {trilateracion} = require('../calculos/trilateracion')
+const {trilateracionMatriz} = require('../calculos/trilateracion2')
 const Graficar = require('../../models/graficar')
 
 
-var d1=0;
-var d2=0;
-var d3=0;
-var x=0;
-var y=0;
+var r1=0;
+var r2=0;
+var r3=0;
+var x1=0;
+var x2=0;
+var x3=0;
+var y1=0;
+var y2=0;
+var y3=0;
 
 let validacion_Trilateracion = async ()=>{
     try{
@@ -121,6 +126,7 @@ let validacion_Trilateracion = async ()=>{
 
         let promesa_puntoXY =(punto, mactag, region)=>{
             return new Promise((resolve, reject)=>{
+                
 
 
                 let graficar = new Graficar({
@@ -154,10 +160,12 @@ let validacion_Trilateracion = async ()=>{
             let resultrpi = await promesa_macrpi(resultRegion[i]._id);
             let resulttag = await promesa_mactag(resultRegion[i]._id);
             // console.log(resultrpi);
+
             // console.log(resulttag);
             // console.log(resultRegion.length);
             
                     for (let k = 0; k < resulttag.length; k++) {// se ejecuta segun la cantidad de tag que existen
+                        let datosPuntoXY = {};
                         for (let j = 0; j < resultrpi.length; j++) {// se ejecuta segun la cantidad
                             // console.log(resultrpi[j]._id);
                             let resultDistancia = await promise_findDistancia(resultrpi[j]._id, resulttag[k]._id);
@@ -167,24 +175,42 @@ let validacion_Trilateracion = async ()=>{
                             let resultadoUbicacion = ubicacion.find(data => data.macRpi === resultrpi[j]._id);
                             // console.log(resultDistancia);
                             if (resultadoUbicacion.axis === 'o') {
-                                d1 = resultDistancia[0].distanciaTag
+                                r1 = resultDistancia[0].distanciaTag;
+                                x1=resultadoUbicacion.xpos;
+                                y1=resultadoUbicacion.ypos;
+
                             }
                             if (resultadoUbicacion.axis === 'x') {
                                 
-                                d2 = resultDistancia[0].distanciaTag
-                                x = resultadoUbicacion.xpos
+                                r2 = resultDistancia[0].distanciaTag;
+                                x2 = resultadoUbicacion.xpos;
+                                y2 = resultadoUbicacion.ypos;
                             }
                             if (resultadoUbicacion.axis === 'y') {
-                                d3 = resultDistancia[0].distanciaTag
-                                y = resultadoUbicacion.ypos
+                                r3 = resultDistancia[0].distanciaTag;
+                                x3 = resultadoUbicacion.xpos;
+                                y3 = resultadoUbicacion.ypos;
                             }
                             
+                            datosPuntoXY= {
+                                x1,
+                                x2,
+                                x3,
+                                y1,
+                                y2,
+                                y3,
+                                r1,
+                                r2,
+                                r3
+                            }
 
 
                         }
                         
-                        let punto =trilateracion(d1, d2, d3, x, y);
-                        // console.log(punto);
+                        let punto =trilateracion(r1, r2, r3, x2, y3);
+                        let punto2 =trilateracionMatriz(datosPuntoXY);
+                        console.log(punto);
+                        console.log(punto2);
 
                         let guardarpuntoXY = await promesa_puntoXY(punto, resulttag[k]._id, resultRegion[i]._id);
                         console.log(guardarpuntoXY.ok);
