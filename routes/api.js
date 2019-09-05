74adc285facae697e12c468cc075141088769b69
betojs <albertoj2504@gmail.com>
2019-09-05 08:17:11 -0400

    cambio de rutas post desde api a post

--- routes/api.js
+++ routes/api.js
@@ -3,31 +3,25 @@ var router = express.Router();
 
 // Require controller modules.
 var apiScanner = require('../controladores/database/scan');
-var apiGuardardb = require('../controladores/database/guardarbd');
 var apiConfigFile= require('../controladores/calculos/configfile/configfile');
 
 // guardar en base de datos desde RASPBERRYPI
+
+
+
 router.get('/dato/:datp',apiScanner.dato)
 
 router.get('/rpi/:rssi/:macBeacon/:macRpi/:date/:beaconType/:sampleId', apiScanner.rpi);
 
-router.post('/rpi/post', apiScanner.dataRaspi);
-
-
-router.post ('/post/constantes', apiGuardardb.constantes);
-router.post ('/post/ubicacion', apiGuardardb.ubicacion);
-router.post ('/post/zona', apiGuardardb.zona);
-
-
 router.get('/configuracion/:mactag/:region/:cantm', apiConfigFile.ejecucionEnSerie);
 
 
-router.get('/constantes/zona/:edif/:piso/:oficina/:tipo', apiConfigFile.guardarzona );
+// router.get('/constantes/zona/:edif/:piso/:oficina/:tipo', apiConfigFile.guardarzona );
 
-router.get('/constantes/ubicacion/:edif/:macrpi', apiConfigFile.guardarubicacion );
+// router.get('/constantes/ubicacion/:edif/:macrpi', apiConfigFile.guardarubicacion );
 
 
-router.get('/rawdatam/:macRpi/:mactag/:distancia', apiScanner.guardarRawDataMuestras );
+// router.get('/rawdatam/:macRpi/:mactag/:distancia', apiScanner.guardarRawDataMuestras );
 
 
 
---
+++ routes/post.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+// Require controller modules.
+var apiScanner = require('../controladores/database/scan');
+var apiGuardardb = require('../controladores/database/guardarbd');
+
+// guardar en base de datos desde RASPBERRYPI
+
+router.post('/rasp_data', apiScanner.dataRaspi);
+
+
+router.post('/rasp_data_c', apiGuardardb.rawCaracterizacion);
+
+
+router.post ('/zona', apiGuardardb.zona);
+router.post ('/constantes', apiGuardardb.constantes);
+router.post ('/ubicacion', apiGuardardb.ubicacion);
+
+
+module.exports = router;
