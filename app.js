caf396db27a8baa3ef15097baf6606efb224c9fe
betojs <albertoj2504@gmail.com>
2019-09-05 08:15:21 -0400

    se agrego la ruta POST

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const inicio = require('./controladores/calculos/timer')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
+var postRouter = require('./routes/post');
 console.log(`aca ES EL APP`);
 // inicio.tiempoEspera();
 var app = express();
@@ -53,6 +54,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
+app.use('/post', postRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
