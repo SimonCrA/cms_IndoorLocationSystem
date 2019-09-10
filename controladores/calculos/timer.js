f865b03effeea0a17b711c35e3c4dd7e6bd18bf4
betojs <albertoj2504@gmail.com>
2019-09-10 03:11:35 -0400

    cambiar su uso original para que realice el llamado de validar

--- controladores/calculos/timer.js
+++ controladores/calculos/timer.js
@@ -3,74 +3,77 @@ const colors = require ('colors');
 const RawData = require('../../models/rawdata');
 
 
+const {validacion_Trilateracion} = require('./validacion');
+
 
 let tiempoEspera = () =>{
-
-    setTimeout(() => {
+    
+    setInterval(() => {
         console.log(`START`.green);
-        tiempoScanner();
-    }, 7000);
+        validacion_Trilateracion();
+        
+    }, 10000);
     
 }
 
 
-let tiempoScanner =async () =>{
-    let cont = 5;
-    let dato = await setTimeout ( async()=>{
-        /* *****************************************
+// let tiempoScanner =async () =>{
+//     let cont = 5;
+//     let dato = await setTimeout ( async()=>{
+//         /* *****************************************
         
-        *	luego de una espera de X segundos
-        *   se inicia el proceso de ubicacion 
-        *   de target en este punto.
+//         *	luego de una espera de X segundos
+//         *   se inicia el proceso de ubicacion 
+//         *   de target en este punto.
         
-        /* ****************************************/
+//         /* ****************************************/
         
-        cont--;
-        try{
-        RawData.aggregate([{"$group":{_id:"$macRpi", count:{$sum:1}}}])
-        .exec(function (err, macrpi){
-            if (err) {
-                console.log(`err`.blue + err);
-            }
-            RawData.aggregate([{"$group":{_id:"$macTag", count:{$sum:1}}}])
-            .exec(function (er, mactag){
-                if (er) {
-                    console.log(`err`.yellow + er);
-                }
+//         cont--;
+//         try{
+//         RawData.aggregate([{"$group":{_id:"$macRpi", count:{$sum:1}}}])
+//         .exec(function (err, macrpi){
+//             if (err) {
+//                 console.log(`err`.blue + err);
+//             }
+//             RawData.aggregate([{"$group":{_id:"$macTag", count:{$sum:1}}}])
+//             .exec(function (er, mactag){
+//                 if (er) {
+//                     console.log(`err`.yellow + er);
+//                 }
 
-                console.log(macrpi.length);
-                console.log(macrpi.length);
-                for (let i= 0;i<macrpi.length;i++){
-                    for(let j=0; j<mactag.length; j++){
-                        try{
-                            filtrar.filtrado( macrpi[i]._id , mactag[j]._id );
+//                 console.log(macrpi.length);
+//                 console.log(macrpi.length);
+//                 for (let i= 0;i<macrpi.length;i++){
+//                     for(let j=0; j<mactag.length; j++){
+//                         try{
+//                             filtrar.filtrado( macrpi[i]._id , mactag[j]._id );
 
-                        }catch(err){
+//                         }catch(err){
 
-                            console.log(`ERROR:`.red + `${err}`);
-                        }
+//                             console.log(`ERROR:`.red + `${err}`);
+//                         }
 
-                    }
-                }
-            });    
+//                     }
+//                 }
+//             });    
 
 
-        });
-    }catch(err){}
+//         });
+//     }catch(err){}
 
         
-    },5000);
-    setTimeout(() => {
-        console.log(`STOP`.red);
-        clearInterval(dato);
-        tiempoEspera();
-        clearTimeout
-    }, 5100);
+//     },5000);
+//     setTimeout(() => {
+//         console.log(`STOP`.red);
+//         clearInterval(dato);
+//         tiempoEspera();
+//         clearTimeout
+//     }, 5100);
 
-}
+// }
 
 
 module.exports = {
-    tiempoEspera,
-    tiempoScanner
+    tiempoEspera
+    // tiempoScanner
 }
