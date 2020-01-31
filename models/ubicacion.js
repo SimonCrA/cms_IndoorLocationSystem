b8912ccaefc5f23fe990e4d5f85d706acc7b08ef
betojs <albertoj2504@gmail.com>
2020-01-31 10:16:50 -0400

    ubicacion of gatewayn and enum

--- models/ubicacion.js
+++ models/ubicacion.js
@@ -2,16 +2,21 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 
+let typeubication = {
+    values: ['BL', 'BR', 'TR', 'TL'],
+    message: '{VALUE}, is no a valid position'
+};
+
 var ubicacionRpiSchema = new Schema({
 
 		macRpi: {type: String},
 		axis: {type: String }, 
-
+		ubicacion:{type: String, enum: typeubication},
         xpos: {type: Number},
 		ypos: {type: Number},
 		idZona:{type: Schema.Types.ObjectId, ref: 'zona'},
 		estatus: {type:Boolean, default: true},
-		compartido:{type:String}
+		compartido:{type: Schema.Types.ObjectId, ref: 'zona'},
 		
 
 	}
