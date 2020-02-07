7b5df8627574ade45397b02310039cea72e2a5e5
betojs <albertoj2504@gmail.com>
2020-02-07 07:53:51 -0400

    tiempo al recibir el activo

--- models/timerToreciveActive.js
+++ models/timerToreciveActive.js
@@ -4,9 +4,9 @@ var Schema = mongoose.Schema;
 
 var timerToreciveActive = new Schema(
     {
-        user: {type: Schema.Types.ObjectId, ref: 'user'},
+        user: {type: Schema.Types.ObjectId, ref: 'User'},
 
-        activo:{type: Schema.Types.ObjectId, ref: 'activo'},
+        activo:{type: Schema.Types.ObjectId, ref: 'Activo'},
         regionPartida:{type: Schema.Types.ObjectId, ref: 'zona'},
         regionLLegada:{type: Schema.Types.ObjectId, ref: 'zona'},
         duracionString:{type: String},
